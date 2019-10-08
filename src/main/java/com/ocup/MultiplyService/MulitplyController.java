65443e86f379bb50e3c79562e62c828e151597e6
Benedikt Grabenmeier <benedikt.grabenmeier@t-systems.com>
2019-10-08 14:36:22 +0200

    change ip

--- src/main/java/com/ocup/MultiplyService/MulitplyController.java
+++ src/main/java/com/ocup/MultiplyService/MulitplyController.java
@@ -14,18 +14,18 @@ public class MulitplyController {
 	@RequestMapping("/multiply")
 	public Double sum(@RequestParam(value = "val1", defaultValue = "0") double val1,
 			@RequestParam(value = "val2", defaultValue = "0") double val2) {
-		logger.debug("Multiply: " + val1 + " * " + val2);
+		//logger.debug("Multiply: " + val1 + " * " + val2);
 		System.out.println("Multiply: " + val1 + " * " + val2);
 
 		RestTemplate restTemplate = new RestTemplate();
 		double result = 0;
 		for (double i = 0; i < val1; i++) {
-			result = restTemplate
-					.getForObject("http://sum-calculator.apps.ca-central-1.starter.openshift-online.com/sum?val1=" + result
-							+ "&val2=" + val2 + "", Double.class);
 //			result = restTemplate
-//					.getForObject("http://172.30.146.95:8080/sum?val1=" + result
+//					.getForObject("http://sum-calculator.apps.ca-central-1.starter.openshift-online.com/sum?val1=" + result
 //							+ "&val2=" + val2 + "", Double.class);
+			result = restTemplate
+					.getForObject("http://172.30.77.162:8080/sum?val1=" + result
+							+ "&val2=" + val2 + "", Double.class);
 //			result = restTemplate.getForObject("http://localhost:8080/sum?val1=" + result + "&val2=" + val2 + "",
 //					Double.class);
 		}
