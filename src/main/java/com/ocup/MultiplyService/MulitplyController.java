10157c6d2ccc41f6505736b7ffc48e92531683f8
Benedikt Grabenmeier <benedikt.grabenmeier@t-systems.com>
2019-10-09 15:29:59 +0200

    minor

--- src/main/java/com/ocup/MultiplyService/MulitplyController.java
+++ src/main/java/com/ocup/MultiplyService/MulitplyController.java
@@ -1,7 +1,5 @@
 package com.ocup.MultiplyService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MulitplyController {
-	Logger logger = LoggerFactory.getLogger(MulitplyController.class);
 
 	@Value("${sumservice}")
 	private String sumServiceUrl;
@@ -18,25 +15,19 @@ public class MulitplyController {
 	@RequestMapping("/multiply")
 	public Double sum(@RequestParam(value = "val1", defaultValue = "0") double val1,
 			@RequestParam(value = "val2", defaultValue = "0") double val2) {
-		//logger.debug("Multiply: " + val1 + " * " + val2);
-		System.out.println("Multiply: " + val1 + " * " + val2);
+		System.out.println("MULTIPLY: " + val1 + " * " + val2);
 
 		RestTemplate restTemplate = new RestTemplate();
 		double result = 0;
 		for (double i = 0; i < val1; i++) {
-//			result = restTemplate
-//					.getForObject("http://sum-calculator.apps.ca-central-1.starter.openshift-online.com/sum?val1=" + result
-//							+ "&val2=" + val2 + "", Double.class);
-//			result = restTemplate
-//					.getForObject("http://172.30.77.162:8080/sum?val1=" + result
-//							+ "&val2=" + val2 + "", Double.class);
-//			result = restTemplate.getForObject("http://localhost:8080/sum?val1=" + result + "&val2=" + val2 + "",
-//					Double.class);
-			
-			result = restTemplate
-					.getForObject(sumServiceUrl+"?val1=" + result
-							+ "&val2=" + val2 + "", Double.class);
+			String servicecall = sumServiceUrl+"?val1=" + result+ "&val2=" + val2;
+			System.out.println("CALL SUM SERVICE: " + servicecall);
+			Double sumResult = restTemplate
+					.getForObject(servicecall, Double.class);
+			System.out.println("SUM RESULT: " + sumResult);
+			result = sumResult;
 		}
+		System.out.println("MULTIPLY RESULT: " + result);
 		return result;
 	}
 }
